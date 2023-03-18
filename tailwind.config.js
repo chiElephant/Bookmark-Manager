0406df120966da496e1efcb2edecda0f3e8ae45b
ChiElephant <anthony.merino@me.com>
2023-03-18 17:19:38 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	content: ['./*.html'],
+	theme: {
+		extend: {
+			colors: {
+				softBlue: 'hsl(231, 69%, 60%)',
+				softRed: 'hsl(0, 94%, 66%)',
+				grayishBlue: 'hsl(229, 8%, 60%)',
+				veryDarkBlue: 'hsl(229, 31%, 21%)',
+			},
+			fontFamily: {
+				sans: ['Rubik', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
